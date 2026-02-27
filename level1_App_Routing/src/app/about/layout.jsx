ead038b01ccb62d63d016dbcd4658fdfb21af824
nakulgadhewal050 <nakulgadhewal050@gmail.com>
2026-02-27 14:05:57 +0530

    first commit

---
+++ level1_App_Routing/src/app/about/layout.jsx
@@ -0,0 +1,10 @@
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        hello layout
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ level1_App_Routing/src/app/layout.js
@@ -0,0 +1,12 @@
+
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        hello layout
+        {children}
+      </body>
+    </html>
+  );
+}
