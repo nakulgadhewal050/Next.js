3d7310ea71ffbe6e7978d4a5f69cb71f905b1957
nakulgadhewal050 <nakulgadhewal050@gmail.com>
2026-02-28 13:34:01 +0530

    font optimization

--- level4-navigatin-image-font/src/app/about/page.jsx
+++ level4-navigatin-image-font/src/app/about/page.jsx
@@ -6,7 +6,10 @@ function page() {
     <div>
  
   <div>
-    <Image src={"/vercel.svg"} width={500} height={500} alt="Vercel" />
+    <Image  src="https://images.unsplash.com/photo-1771777400683-8614965bfce5?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8"
+     width={500} 
+     height={500} 
+     alt="Vercel" />
   </div>
     </div>
   )
--- level4-navigatin-image-font/src/app/layout.js
+++ level4-navigatin-image-font/src/app/layout.js
@@ -1,12 +1,20 @@
-
+import { Inter } from "next/font/google";
 import "./globals.css";
+
+
+const poppins = Inter({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  display: 'swap'
+})
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={poppins.className}>
       <body>
         {children}
       </body>
     </html>
   );         
-}
+}           
+                     
